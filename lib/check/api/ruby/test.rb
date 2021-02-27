c4de2f5d5d683b3329298053470e909ccce8c654
Nikita Naumenko <naumenkoniki@gmail.com>
2021-02-27 11:27:50 +0300

    check api ruby test

---
+++ lib/check/api/ruby/test.rb
@@ -0,0 +1,12 @@
+require "check/api/ruby/test/version"
+
+module Check
+  module Api
+    module Ruby
+      module Test
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/check/api/ruby/test/version.rb
@@ -0,0 +1,9 @@
+module Check
+  module Api
+    module Ruby
+      module Test
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
